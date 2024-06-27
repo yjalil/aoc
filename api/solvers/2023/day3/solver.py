d52dfc5608e3c2c8227fca97a6dddf20bb2807fd
abdeljalil yahya <jalil.yahy@gmail.com>
2024-06-27 03:14:43 +0100

    2023day3

--- api/solvers/2023/day3/solver.py
+++ api/solvers/2023/day3/solver.py
@@ -24,4 +24,24 @@ def part1(input):
     return sum([int(x) for x in total])
 
 def part2(input):
-    pass
+    p = r'(\d+)'
+    pp =  r'(\d+)\.{1,5}\*\.{2,4}(\d+)'
+
+    total = []
+    padded_input = ['.' + line + '.' for line in input]
+    for id,line in enumerate(padded_input[:-2]):
+        print(f'==========line{id + 1 }============')
+        nums = re.findall(p,line)
+        start = 0
+        for num in nums :
+            window = padded_input[id][line.index(num, start):line.index(num, start)+len(num)] + '.' \
+                + padded_input[id + 1][line.index(num, start)-1:line.index(num, start)+len(num) + 1] \
+                + padded_input[id + 2][line.index(num, start)-4:line.index(num, start)+len(num) + 3]
+            print(f'number: {num} with window {window} => {re.findall(pp,window)}')
+            match = re.findall(pp,window)
+            if len(match) > 0 :
+                total.append(int(match[0][0])*int(match[0][1]))
+            start = line.index(num) + len(num)
+        print(f'====>{total}')
+    # return total
+    return sum([int(x) for x in total])
--- api/tests/solvers/2023/test_day3.py
+++ api/tests/solvers/2023/test_day3.py
@@ -5,6 +5,6 @@ def test_day3_2023_part1():
     assert solve(2023,3,1,'input') == 531932
 
 
-# def test_day3_2023_part2():
-#     assert solve(2023,3,2,'example2') == 467835
-#     assert solve(2023,3,2,'input') == 73646890
+def test_day3_2023_part2():
+    assert solve(2023,3,2,'example2') == 467835
+    assert solve(2023,3,2,'input') == 73646890
