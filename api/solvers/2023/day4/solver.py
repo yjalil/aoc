077c51a57e6bd579201b2d83d2f00722538a265a
abdeljalil yahya <jalil.yahy@gmail.com>
2024-06-28 12:18:24 +0100

    2023day4

--- api/solvers/2023/day4/solver.py
+++ api/solvers/2023/day4/solver.py
@@ -15,24 +15,52 @@ def part1(input):
         # print(Round(winners,haves,score))
     return sum(round.score for round in rounds)
 
+# ==========================Blocks Memory=======================
+# def part2(input):
+#     p = r'(\d+)'
+#     Round = namedtuple('Round', ['card','wins','haves','matches'])
+#     rounds = []
+#     cards = defaultdict(int)
+#     for line in input:
+#         card = line.split(':')[0]
+#         line = line.split(':')[-1]
+#         winners = re.findall(p,line.split('|')[0])
+#         haves = re.findall(p,line.split('|')[-1])
+#         matches  = len(set(winners).intersection(set(haves)))
+#         rounds.append(Round(card,winners,haves,matches))
+#     cards = defaultdict(int)
+#     for round in (rounds):
+#         cards[round.card] += 1
+#     cards[rounds[0].card] = 1
+#     for i,r in enumerate(rounds):
+#         for card in rounds[i+1:i+int(r.matches)+1] :
+#             for count in range(cards[rounds[i].card]) :
+#                 cards[card.card] += 1
+
+#     return sum(cards.values())
+
+# ===================With generators=====================
 def part2(input):
     p = r'(\d+)'
-    Round = namedtuple('Round', ['card','wins','haves','matches'])
+    Round = namedtuple('Round', ['card', 'wins', 'haves', 'matches'])
+
     rounds = []
     cards = defaultdict(int)
+
     for line in input:
         card = line.split(':')[0]
         line = line.split(':')[-1]
-        winners = re.findall(p,line.split('|')[0])
-        haves = re.findall(p,line.split('|')[-1])
-        matches  = len(set(winners).intersection(set(haves)))
-        rounds.append(Round(card,winners,haves,matches))
-        # print(Round(card,winners,haves,matches))
-    c = len(rounds)
-    print(c)
-    for i,r in enumerate(rounds):
-        for next_card in rounds[i+1:i+int(r.matches)+1]:
-            print(f'At {r.card} : {r.matches} ====> add copy of {next_card}')
-            c += int(next_card.matches)
-            print(c)
-    return c
+        winners = re.findall(p, line.split('|')[0])
+        haves = re.findall(p, line.split('|')[-1])
+        matches = len(set(winners).intersection(set(haves)))
+        rounds.append(Round(card, winners, haves, matches))
+
+    for round in rounds:
+        cards[round.card] += 1
+
+    for i, r in enumerate(rounds):
+        for card in rounds[i+1:i+int(r.matches)+1]:
+            for _ in range(cards[rounds[i].card]):
+                cards[card.card] += 1
+
+    return sum(cards.values())
